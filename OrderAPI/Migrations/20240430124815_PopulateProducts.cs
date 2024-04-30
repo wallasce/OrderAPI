31a39c655f00124a55e47e904fcd3567a09409df
Wallasce Leite <lwallasce@outlook.com>
2024-04-30 09:54:01 -0300

    Data: Populate Products

---
+++ OrderAPI/Migrations/20240430124815_PopulateProducts.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulateProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO products(CategoryID, Name, Description, Price, Serves) " + 
+                "VALUES(1, 'Dadinho de Tapioca', 'Acompanha Geleia de Pimenta', 30, 2)");
+            mb.Sql("INSERT INTO products(CategoryID, Name, Description, Price, Serves) " +
+                "VALUES(2, 'Guarana Antártica Zero', 'Lata de 350ml', 5, 1)");
+            mb.Sql("INSERT INTO products(CategoryID, Name, Description, Price, Serves) " +
+                "VALUES(3, 'Cheesecake', 'Autentica torta gelada americana', 15, 1)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM products");
+        }
+    }
+}
