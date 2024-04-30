d8bd9b10b9a03b9b31d329581f8847a98a532f11
Wallasce Leite <lwallasce@outlook.com>
2024-04-30 08:32:15 -0300

    Feat: Create Order Entity

---
+++ OrderAPI/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+
+namespace OrderAPI.Models;
+
+public class Order
+{
+    public int OrderId { get; set; }
+    public long CPF { get; set; }
+    public int Tabble { get; set; }
+    
+    public enum States
+    {
+        Open,
+        Close,
+    }
+    public States State { get; set; }
+
+    public ICollection<Item> Items { get; set; }
+
+    public Order()
+    {
+        Items = new Collection<Item>();
+    }
+
+    public float CurrentOrderBalance { get; set; }
+}
