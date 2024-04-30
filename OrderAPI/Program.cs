64f6838d7ae25b970d6c26ab16b0575711aabfbe
Wallasce Leite <lwallasce@outlook.com>
2024-04-30 13:32:34 -0300

    Chore: Avoid Cycles in Include

--- OrderAPI/Program.cs
+++ OrderAPI/Program.cs
@@ -1,12 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using OrderAPI.Context;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers().AddJsonOptions(options =>
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
