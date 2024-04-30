1d700627def1514a8c296cb8f0c3552242ead878
Wallasce Leite <lwallasce@outlook.com>
2024-04-30 08:28:09 -0300

    Feat: Create Entity Product

---
+++ OrderAPI/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿namespace OrderAPI.Models;
+
+public class Item
+{
+    public int ItemID { get; set; }
+    public Product Product { get; set; }
+    public int Quantity { get; set; }
+    public string Observation { get; set; }
+
+    public enum States
+    {
+        PREPARING,
+        READY,
+        DELIVERED,
+    }
+
+    public States state { get; set; }
+}
