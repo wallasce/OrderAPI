4880630c76941e3c47b34fefedb7f2316de5f44f
Wallasce Leite <lwallasce@outlook.com>
2024-05-02 10:08:48 -0300

    Feat: Create Filter to avoid uncatch exception.

---
+++ OrderAPI/Filters/ApiExceptionFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace OrderAPI.Filters;
+
+public class ApiExceptionFilter : IExceptionFilter
+{
+    private readonly ILogger<ApiExceptionFilter> _logger;
+
+    public ApiExceptionFilter(ILogger<ApiExceptionFilter> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnException(ExceptionContext context)
+    {
+        _logger.LogError(context.Exception, "An unhandled exception occurred: Status Code 500");
+
+        context.Result = new ObjectResult("A problem occurred while handling your solution: Status Code 500")
+        {
+            StatusCode = StatusCodes.Status500InternalServerError,
+        };
+    }
+}
