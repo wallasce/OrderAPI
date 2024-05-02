2e1a5e4e6a189185b983ffae7766117ebcf38ab1
Wallasce Leite <lwallasce@outlook.com>
2024-05-02 15:42:28 -0300

    Fix: Create service to use generic repository

--- OrderAPI/Program.cs
+++ OrderAPI/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using OrderAPI.Context;
 using OrderAPI.Extensions;
 using OrderAPI.Filters;
+using OrderAPI.Interfaces;
+using OrderAPI.Repositories;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -26,6 +28,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
                     ServerVersion.AutoDetect(mySqlConnection)));
 
 builder.Services.AddScoped<ApiLoggingFilter>();
+
+builder.Services.AddScoped(typeof(ICRUDRepository<>), typeof(CRUDRepository<>));
 
 var app = builder.Build();
 
