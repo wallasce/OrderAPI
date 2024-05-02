5c261af8c62f4ab631640a2e170da396adae716e
Wallasce Leite <lwallasce@outlook.com>
2024-05-02 13:22:50 -0300

    Feat: Create generic repository CRUD

---
+++ OrderAPI/Repositories/IRepositoryCRUDBase.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderAPI.Repositories;
+
+public interface IRepositoryCRUDBase<T>
+{
+    IEnumerable<T> GetAll();
+    T Get(int id);
+    T Create(T entity);
+    T Update(T entity);
+    T Delete(T entity);
+}
