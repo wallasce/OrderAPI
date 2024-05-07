240fe6037947a9e7b732b9f1b9736ea6e41e1815
Wallasce Leite <lwallasce@outlook.com>
2024-05-07 11:45:41 -0300

    Feat: Create Category DTO

---
+++ OrderAPI/DTOs/CategoryDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderAPI.DTOs;
+
+public class CategoryDTO
+{
+    public int CategoryId { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string Name { get; set; }
+}
