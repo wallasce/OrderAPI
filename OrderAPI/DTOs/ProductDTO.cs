ced5772ccf873981dcaee89e0e685ad00a0ca24b
Wallasce Leite <lwallasce@outlook.com>
2024-05-07 11:45:56 -0300

    Feat: Create Product DTO

---
+++ OrderAPI/DTOs/ProductDTO.cs
@@ -0,0 +1,28 @@
+﻿using OrderAPI.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace OrderAPI.DTOs;
+
+public class ProductDTO
+{
+    public int ProductId { get; set; }
+
+    [Required]
+    public int CategoryId { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string Name { get; set; }
+
+    [Required]
+    [StringLength(200)]
+    public string Description { get; set; }
+
+    [Required]
+    public float Price { get; set; }
+
+    [Required]
+    public int Serves { get; set; }
+}
